4266c6246393b4e5e8229fca713613cdf6402ed1
carldelacruz <carl_delacruz@dlsu.edu.ph>
2021-08-16 23:03:02 +0800

    Added navigation for home and createmap

--- app/src/main/java/com/mobdeve/s18/delacruz/carl/mcotemp/CreatemapActivity.java
+++ app/src/main/java/com/mobdeve/s18/delacruz/carl/mcotemp/CreatemapActivity.java
@@ -1,2 +1,30 @@
-package com.mobdeve.s18.delacruz.carl.mcotemp;public class CreatemapActivity {
+package com.mobdeve.s18.delacruz.carl.mcotemp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mobdeve.s18.delacruz.carl.mcotemp.databinding.ActivityCreatemapBinding;
+
+public class CreatemapActivity extends AppCompatActivity {
+    private ActivityCreatemapBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCreatemapBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnCreatemapBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoHome = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(gotoHome);
+            }
+        });
+    }
 }
--- app/src/main/java/com/mobdeve/s18/delacruz/carl/mcotemp/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s18/delacruz/carl/mcotemp/HomeActivity.java
@@ -2,13 +2,28 @@ package com.mobdeve.s18.delacruz.carl.mcotemp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.mobdeve.s18.delacruz.carl.mcotemp.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnHomeCreatemap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoCreateMap = new Intent(getApplicationContext(), CreatemapActivity.class);
+                startActivity(gotoCreateMap);
+            }
+        });
     }
 }
