dd5de0f973d5d2e618802ac02cbedb14baa9751c
carldelacruz <carl_delacruz@dlsu.edu.ph>
2021-08-18 19:53:23 +0800

    Added popupwindow in createmap and popup view

--- app/src/main/java/com/mobdeve/s18/delacruz/carl/mcotemp/CreatemapActivity.java
+++ app/src/main/java/com/mobdeve/s18/delacruz/carl/mcotemp/CreatemapActivity.java
@@ -2,15 +2,21 @@ package com.mobdeve.s18.delacruz.carl.mcotemp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mobdeve.s18.delacruz.carl.mcotemp.databinding.ActivityCreatemapBinding;
 
 public class CreatemapActivity extends AppCompatActivity {
     private ActivityCreatemapBinding binding;
+    private LayoutInflater layoutInflater;
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,18 @@ public class CreatemapActivity extends AppCompatActivity {
         binding.btnCreatemapBack.setOnClickListener(v -> {
             Intent gotoHome = new Intent(getApplicationContext(), HomeActivity.class);
             startActivity(gotoHome);
+        });
+
+        binding.btnCreatemap00.setOnClickListener(v -> {
+            layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = layoutInflater.inflate(R.layout.activity_createmap_popup, null);
+
+            int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            boolean focusable = true; // lets taps outside the popup also dismiss it
+            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
         });
     }
 }
